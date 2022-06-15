44ed3053dd2a7d27db7d727da8dbc98972bbff93
lidengxiang <495234450@qq.com>
2022-06-15 17:31:39 +0800

    CustomUserDetails添加无参构造

--- src/main/java/com/ldx/JWTsecurity/pojo/CustomUserDetails.java
+++ src/main/java/com/ldx/JWTsecurity/pojo/CustomUserDetails.java
@@ -2,12 +2,14 @@ package com.ldx.JWTsecurity.pojo;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class CustomUserDetails implements UserDetails {
     private User user;
